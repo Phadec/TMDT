edbb42699889e9ffe242e753bb2b4c821d420cbf
khainam23 <khainam23@gmail.com>
2025-06-23 10:39:41 +0700

    Tối ưu lại promt

--- src/promt.jsx
+++ src/promt.jsx
@@ -1,6 +1,6 @@
 const handleContent = (content) => {
   return `
-    Dưới đây là nội dung của một bài viết quảng cáo. Hãy kiểm tra và đánh giá tính hiệu quả, mức độ hấp dẫn và tính thuyết phục của nội dung này. Nếu có thể, hãy đề xuất một phiên bản cải thiện tốt hơn, không vượt quá 1000 ký tự. Phản hồi một cách chuyên nghiệp, súc tích và mang tính marketing cao.
+    Dưới đây là nội dung của một bài viết rao vặt. Hãy kiểm tra và đánh giá tính hiệu quả, mức độ hấp dẫn và tính thuyết phục của nội dung này. Nếu có thể, hãy đề xuất một phiên bản cải thiện tốt hơn, không vượt quá 1000 ký tự. Phản hồi một cách chuyên nghiệp, súc tích và mang tính marketing cao.
     Đây là nội dung:
     ${content}
 `;
