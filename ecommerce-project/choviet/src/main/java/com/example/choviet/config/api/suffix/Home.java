cfbe3bee630b55c7ef9f598a3d5a24d942117f3c
khainam23 <khainam23@gmail.com>
2025-06-24 00:14:17 +0700

    thêm xử lý cho sản phẩm tốt hôm nay

--- ecommerce-project/choviet/src/main/java/com/example/choviet/config/api/suffix/Home.java
+++ ecommerce-project/choviet/src/main/java/com/example/choviet/config/api/suffix/Home.java
@@ -3,4 +3,5 @@ package com.example.choviet.config.api.suffix;
 public interface Home {
     String HOME = "/home";
     String BANNER = "/banner";
+    String TODAY_RECOMMENDATIONS = "/today-recommendations";
 }
